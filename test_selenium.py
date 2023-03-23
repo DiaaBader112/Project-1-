240b0b24a82e33dad31c2a0d6f7db47c67e66134
DiaaBader112 <128405261+DiaaBader112@users.noreply.github.com>
2023-03-24 01:27:30 +0200

    Add files via upload

---
+++ test_selenium.py
@@ -0,0 +1,77 @@
+import time
+
+import pytest
+from main import *
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import requests
+
+
+
+#______________________________Sanity Test 100%__________________________________________________
+class TestSanity_UI:
+    @pytest.fixture
+    def driver(self):
+        driver = webdriver.Chrome()
+        return driver
+
+    def test_sanity(self,driver):
+        expected = 'https://www.saucedemo.com/'
+        actual = open_site(driver,expected)
+        assert expected == actual
+#______________________________Login Test 100%____________________________________________________
+class TestLogin_UI:
+    @pytest.fixture
+    def driver(self):
+        driver = webdriver.Chrome()
+        return driver
+    @pytest.fixture()
+    def url(self):
+        return 'https://www.saucedemo.com/'
+
+    def test_login(self,driver,url):
+        expected = 'https://www.saucedemo.com/inventory.html'
+        open_site(driver , url)
+        actual = login(driver,'standard_user','secret_sauce')
+        assert expected == actual
+#________________________________Login_N Test 100%_________________________________________________
+    @pytest.fixture()
+    def url(self):
+        return 'https://www.saucedemo.com/'
+
+    def test_login_N(self,driver,url):
+        expected = 'https://www.saucedemo.com/inventory.html'
+        open_site(driver , url)
+        actual = login_N(driver,'diaa' , '123')
+        assert expected == actual
+        print(driver.current_url)
+#____________________________________________________________________________________________________
+    def test_images(self,driver):
+        expected = True
+        open_site(driver,url)
+        actual = Imagescheck(driver)
+        assert expected == actual
+#_______________________________________________________________________________________________________
+    # def test_Price(self,driver):
+    #     expected = True
+    #     open_site(driver,url)
+    #     actual = Price(driver)
+    #     assert expected == actual
+
+
+
+
+
+
+
+
+class TestApi:
+    @pytest.fixture
+    def driver(self):
+        driver = webdriver.Chrome()
+        return driver
+
+    def test_sanity_API(self,driver):
+        res = requests.get('https://reqres.in/api/users')
+        assert res.status_code <400 and res.status_code >= 200
+        print(f' The Status Code you received {res.status_code}')
